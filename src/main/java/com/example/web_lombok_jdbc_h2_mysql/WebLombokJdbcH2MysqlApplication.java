7d94684dd771f9dc6448bad8ef8364535f684105
narayansau <narayansau@yahoo.com>
2019-07-14 19:37:07 +0530

    Initial commit

---
+++ src/main/java/com/example/web_lombok_jdbc_h2_mysql/WebLombokJdbcH2MysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_lombok_jdbc_h2_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLombokJdbcH2MysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebLombokJdbcH2MysqlApplication.class, args);
+    }
+
+}
