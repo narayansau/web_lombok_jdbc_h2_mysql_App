ffb2dd2643144f35520f215fefa0ddce76faae6c
narayansau <narayansau@yahoo.com>
2019-07-15 10:37:36 +0530

    Correct Implementation of LOMBOK , JDBC , H2, MySQL

--- src/main/java/com/example/web_lombok_jdbc_h2_mysql/Interfaces/CommonRepository.java
+++ src/main/java/com/example/web_lombok_jdbc_h2_mysql/Interfaces/CommonRepository.java
@@ -1,8 +1,13 @@
 package com.example.web_lombok_jdbc_h2_mysql.Interfaces;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.Collection;
 
-public interface CommonRepository<T> {
+@Repository
+public interface CommonRepository<T>   {
+
+
 
     public T save(T domain);
     public  Iterable<T> save(Collection<T> domains);
